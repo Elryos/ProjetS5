fb0147b3bbe9d3885c0d8db87259e76d8af6d33b
Antoine <antoine26.guerin@gmail.com>
2017-12-20 15:57:07 +0100

    LDR_STR/B/H

--- arm_simulator-1.4/arm_load_store.c
+++ arm_simulator-1.4/arm_load_store.c
@@ -23,43 +23,124 @@ Contact: Guillaume.Huard@imag.fr
 #include "arm_load_store.h"
 #include "arm_exception.h"
 #include "arm_constants.h"
+#include "arm_data_processing.h"
+#include "arm_instruction.h"
 #include "util.h"
 #include "debug.h"
 
 #define LS 0b1 <<20
 #define BYTE 0b1 <<22
+#define IMM_REG 0b1 <<22
 #define HALF 0b1001 <<4
 #define ADR_MOD 0b11001 <<21
+#define U 0b1 <<23
+#define Rn 0b1111 <<16
+#define Rd 0b1111 <<12
+#define offset_12 0xFFF
+#define IMMH 0b1111 <<8
+#define IMML 0b1111
+#define Rm 0b1111
 
-#define IMM_PRE 0b01
-#define REG_SCA_PRE 0b11
-#define IMM_POS 0b00
-#define REG_SCA_POS 0b10
+#define IMM 0b0
+#define REG_SCA 0b1
 
-int arm_load_store(arm_core p, uint32_t ins) {	
-	/*int res;
-	if ((ins & HALF >> 4) == 0b1001){
-		if(ins & LS >>20){
-			res=arm_LDRH(p,ins);
+
+int arm_LDR_STR (arm_core p,uint32_t ins){
+	uint32_t address;
+
+	if (ins & U >> 23){
+		if((ins & ADR_MOD >> 25) == IMM){
+			address = arm_read_register(p,ins & Rn >>20) + (ins & offset_12);
 		}
 		else{
-			res=arm_STRH(p,ins);
+			address = arm_read_register(p,ins & Rn >> 20) + shifter_operand(p,ins);
 		}
 	}
 	else{
-	    switch(ins & BYTE >>22){
-	    	case(1) :
-	    		res=arm_LDRB_STRB(p,ins);
-	    		break;
-	    	case(0) :
-	    		res=arm_LDR_STR(p,ins);
-	    		break;
-	    }
-	}*/
-    return UNDEFINED_INSTRUCTION;
+		if((ins & ADR_MOD >> 25) == IMM){
+			address = arm_read_register(p,ins & Rn >>20) - (ins & offset_12);
+		}
+		else{
+			address = arm_read_register(p,ins & Rn >> 20) - shifter_operand(p,ins);
+		}
+	}
+	arm_write_register(p,ins & Rn >>16,address);
+	if(ins & BYTE >>22){
+		uint8_t bvalue;
+		if(ins & LS >>20){
+			if(arm_read_byte(p,address,&bvalue)){
+				arm_write_register(p,ins & Rd >>12,bvalue);
+				return 0;
+			}
+			return 1;
+		}
+		else{
+			bvalue=arm_read_register(p,ins & Rd >>12);
+			return arm_write_byte(p,address,bvalue);
+		}
+	}
+	else{
+		uint32_t value;
+		if (ins & LS >>20){
+			if (arm_read_word(p,address,&value)){
+				arm_write_register(p,ins & Rd >>12,value);
+				return 0;
+			}
+			return 1;
+		}
+		else{
+			value = arm_read_register(p,ins & Rd >>12);
+			return arm_write_word(p,address,value);
+		}
+	}		
+}
+
+int arm_LDRH_STRH (arm_core p,uint32_t ins){
+	uint32_t address;
+	if (ins & U >> 23){
+		if(ins & IMM_REG >> 22){
+			address = arm_read_register(p,ins & Rn >> 20) + (ins & IMMH >> 4) + (ins & IMML);
+		}
+		else{
+			address = arm_read_register(p,ins & Rn >> 20) + arm_read_register(p,ins & Rm);
+		}
+	}
+	else{
+		if (ins & IMM_REG >> 22){
+			address = arm_read_register(p,ins & Rn >> 20) - (ins & IMMH >> 4) + (ins & IMML);
+		}
+		else{
+			address = arm_read_register(p,ins & Rn >> 20) - arm_read_register(p,ins & Rm);
+		}
+	}
+	arm_write_register(p,ins & Rn >>16,address);
+	uint16_t value;
+	if (ins & LS >> 20){
+		if (arm_read_half(p,address,&value)){
+			arm_write_register(p,ins & Rd >>12,value);
+			return 0;
+		}
+		return 1;
+	}
+	else {
+		value = arm_read_register(p,ins & Rd >>12);
+		return arm_write_half(p,address,value);
+	}
+}
+
+int arm_load_store(arm_core p, uint32_t ins) {	
+	int res;
+	if ((ins & HALF >> 4) == 0b1001){
+		res=arm_LDRH_STRH(p,ins);
+	}
+	else{
+		res=arm_LDR_STR(p,ins);
+	}
+    return res;
 }
 
 int arm_load_store_multiple(arm_core p, uint32_t ins) {
+
     return UNDEFINED_INSTRUCTION;
 }
 
@@ -68,10 +149,3 @@ int arm_coprocessor_load_store(arm_core p, uint32_t ins) {
     return UNDEFINED_INSTRUCTION;
 }
 
-/*int arm_LDR_STR (arm_core p,uint32_t ins){
-	switch(ins & ADR_MOD){
-		case(IMM_PRE) :
-			
-	}
-}
-*/
