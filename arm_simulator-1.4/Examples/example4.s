585ede64cce7a30e19bef457e86ea15527e4ea95
VincentPinet <vincent.pinet@gmail.com>
2018-01-12 15:18:45 +0100

    VOIAL

--- arm_simulator-1.4/Examples/example4.s
+++ arm_simulator-1.4/Examples/example4.s
@@ -9,6 +9,13 @@ main:
     ldr r5, [r1, +r4, lsl #2]
     ldmia r1!, {r6, r7}
     swi 0x123456
+
+    MOVS r1, #0xFFFFFFFF
+    MOVS r2, #1
+    MOV R3, r2, LSL #31
+    SUBS r4, r1, r3
+    swi 0x123456
+
 .data
 donnee:
     .word 0x11223344
